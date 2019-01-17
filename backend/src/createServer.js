d1f66ae14d59600fddb4afac7d67ea705ac1e9a0
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-01-17 11:11:52 +0100

    Ready to start: database, server and schema.graphql

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,3 +1,22 @@
 const { graphQLServer } = require("graphql-yoga");
 const Mutation = require("./resolvers/Mutation");
 const Query = require("./resolvers/Query");
+const db = require("./db");
+
+//Create the GraphQL Yoga Server
+
+function CreateServer() {
+  return new graphQLServer({
+    typeDefs: "src/schema.graphql",
+    resolvers: {
+      Mutation,
+      Query
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({ ...req, db })
+  });
+}
+
+module.exports = createServer;
