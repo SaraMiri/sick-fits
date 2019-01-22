2ef80811190ca06609244325bd7a5008d72e1ad3
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-01-22 14:07:04 +0100

    Add FormatMoney and images to <Item> Component

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import Link from "next/link";
 import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
+import formatMoney from "../lib/formatMoney";
 
 class Item extends React.Component {
   static propTypes = {
@@ -12,7 +14,32 @@ class Item extends React.Component {
     const { item } = this.props;
     return (
       <ItemStyles>
-        <Title>{item.title}</Title>
+        {item.image && <img src={item.image} alt={item.title} />}
+        <Title>
+          <Link
+            href={{
+              pathname: "./item",
+              query: { id: item.id }
+            }}
+          >
+            <a>{item.title}</a>
+          </Link>
+        </Title>
+        <PriceTag>{formatMoney(item.price)}</PriceTag>
+        <p>{item.description}</p>
+
+        <div classname="buttonList">
+          <Link
+            href={{
+              pathname: "update",
+              query: { id: item.id }
+            }}
+          >
+            <a>Edit ✏</a>
+          </Link>
+          <button>Add to Cart</button>
+          <button>Delete</button>
+        </div>
       </ItemStyles>
     );
   }
