c46bf6736676510603ecb3599f653a320093ecd1
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-01-22 08:34:11 +0100

    Frontend: created <items> and <Items> components.

---
+++ frontend/components/Items.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Query } from "react-apollo";
+
+class Items extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Items</p>
+      </div>
+    );
+  }
+}
+
+export default Items;
