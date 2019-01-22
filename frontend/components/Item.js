77ae73d57bc349ac5494bd68e3a8e7e7324c72ae
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-01-22 13:35:04 +0100

    WIP: Error "Cannot found ./styles" fix

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import Title from "./styles";
+import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 
 class Item extends React.Component {
   static propTypes = {
-    item: propTypes.object.isRequired
+    item: PropTypes.object.isRequired
   };
   render() {
     const { item } = this.props;
