081fb2f9f2b53908e7e24d5625a3ae6cc13e4aaf
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-01-25 11:09:51 +0100

    Add Router and Error to <CreateItem> and other stuff

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import Router from "next/router";
 
 import Form from "./styles/Form";
 import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -45,12 +47,21 @@ class CreateItem extends React.Component {
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
           <Form
-            onSubmit={event => {
+            onSubmit={async event => {
+              // Stop the form from submitting
               event.preventDefault();
-              console.log(this.state);
+              // Call the mutation
+              const res = await createItem();
+              // Change them to single item page
+              console.log(res);
+              Router.push({
+                pathname: "/item",
+                query: { id: res.data.createItem.id }
+              });
             }}
           >
-            <fieldset>
+            <Error error={error} />
+            <fieldset disabled={loading} aria-busy={true}>
               <label htmlFor="title">
                 Title
                 <input
